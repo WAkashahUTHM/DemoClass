ca1bd888c21dab885b0b8dafef000259d18be00d
Wan Akashah <wanakashah14833@gmail.com>
2025-04-11 07:43:48 +0800

    Update: Change dashboard text

--- techgrc.client/src/pages/ContractDashboardPage.tsx
+++ techgrc.client/src/pages/ContractDashboardPage.tsx
@@ -39,7 +39,7 @@ const ContractDashboardPage: React.FC = () => {
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 <Typography variant="h4" gutterBottom>
-                   Dashboard
+                   Dashboard and Hello
                 </Typography>
             </Grid>
 
